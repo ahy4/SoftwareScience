b31b9d98c4b6b6af9da30d3ec1a99507e587bc6d
yuya sato <mrfool38@gmail.com>
2016-07-10 18:41:05 +0900

    initial commit

---
+++ src/SoftwareScience/R1414048.java
@@ -0,0 +1,21 @@
+package SoftwareScience;
+
+/**
+ * Created by noko on 2016/07/10.
+ */
+public class R1414048
+{
+    public static void main(String[] args)
+    {
+        try
+        {
+            Score[] scores = ParseData.parseData(FileManager.readFileSync("/Users/noko/Desktop/Seiseki.dat"));
+            System.out.println(new ScoreAssessment(scores));
+        } catch (Exception e)
+        {
+            System.out.println("ファイル読み込みエラー");
+            e.printStackTrace();
+        }
+    }
+}
+
